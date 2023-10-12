eae90f909f270fde6ecac0564bdbd9fb78660948
zaragood <azaragoodness52@gmail.com>
2023-10-12 22:33:40 +0000

    created an instance

--- console.py
+++ console.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
 """file that controls the entry point of command interpreter"""
 
 
 class HBNBCommand(cmd.Cmd):
     """a custom prompt"""
+    __classes = ("BaseModel",)
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
@@ -19,6 +21,17 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """shouldn’t execute anything"""
         return
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_obj = eval(f"{args[0]}")()
+            print(new_obj.id)
 
 
 if __name__ == '__main__':
