6054198baf50a0aec5a8ff4dc634466bd26bd77b
zaragood <azaragoodness52@gmail.com>
2023-10-13 21:42:49 +0000

    not instance attaibute but class attributes

--- models/user.py
+++ models/user.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """This module contains the User class that inherits
 from the BaseModel class"""
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """The user class inherits from BaseModel class"""
-    def __init__(self):
-        """initializing the user class"""
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+        """Public class attributes"""
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
