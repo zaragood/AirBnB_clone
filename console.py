a26a6cebeb07ca6c3dea87ce4c1ff535d38c9bd5
zaragood <azaragoodness52@gmail.com>
2023-10-13 21:41:42 +0000

    updated console.py with update command

--- console.py
+++ console.py
@@ -2,12 +2,13 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
 """file that controls the entry point of command interpreter"""
 
 
 class HBNBCommand(cmd.Cmd):
     """a custom prompt"""
-    __classes = ("BaseModel",)
+    __classes = ("BaseModel", "User",)
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
@@ -49,6 +50,53 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
         else:
             print(storage.all()[f"{args[0]}.{args[1]}"])
+
+    def do_destroy(self, arg):
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print("** no instance found **")
+        else:
+            del storage.all()[f"{args[0]}.{args[1]}"]
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances
+        based or not on the class name
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print([str(value) for value in storage.all().values()])
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            dic = storage.all().items()
+            print([str(val) for key, val in dic if key.startswith(args[0])])
+
+    def do_update(self, args):
+        """"""
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print("** no instance found **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+
 
 
 if __name__ == '__main__':
