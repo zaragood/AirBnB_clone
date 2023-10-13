2b59d8bbb55342380c692a321f230f6e1a0de1b9
zaragood <azaragoodness52@gmail.com>
2023-10-13 19:29:34 +0000

    updating console.py with create, show

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models import storage
 """file that controls the entry point of command interpreter"""
 
 
@@ -32,6 +33,22 @@ class HBNBCommand(cmd.Cmd):
         else:
             new_obj = eval(f"{args[0]}")()
             print(new_obj.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+        based on the class name and id
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print("** no instance found **")
+        else:
+            print(storage.all()[f"{args[0]}.{args[1]}"])
 
 
 if __name__ == '__main__':
