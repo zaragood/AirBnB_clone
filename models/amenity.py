a20568ce72ccf71a4f0260123f341f1f55d1dab4
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-14 00:56:53 -0700

    Amenity class added

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains Amenity class that inherit from BaseModel"""
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """City Class attributes """
+    state_id = ""
+    name = ""
