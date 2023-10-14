c4ebbc36ab07b3fd6d2a89f113940bb84a6b9c35
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-14 00:49:22 -0700

    City Class added

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains City class that inherit from BaseModel"""
+from base_model import BaseModel
+
+
+class City(BaseModel):
+    """City Class attributes """
+    state_id = ""
+    name = ""
