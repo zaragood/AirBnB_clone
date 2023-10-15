36b8ea12fa397b1dc4a4d06d207ba0d501eb4c01
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-15 07:20:16 -0700

    test case for place

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""test file for Place Class"""
+import unittest
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """Test case for Place Class"""
+
+    def test_on_default(self):
+        """test for default value"""
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
+
+    def test_type(self):
+        """Check the type of each place attributes"""
+        place = Place()
+
+        self.assertIsInstance(place.city_id, str)
+        self.assertIsInstance(place.user_id, str)
+        self.assertIsInstance(place.name, str)
+        self.assertIsInstance(place.description, str)
+        self.assertIsInstance(place.number_rooms, int)
+        self.assertIsInstance(place.number_bathrooms, int)
+        self.assertIsInstance(place.max_guest, int)
+        self.assertIsInstance(place.price_by_night, int)
+        self.assertIsInstance(place.latitude, float)
+        self.assertIsInstance(place.amenity_ids, list)
+
+    def test_attribute(self):
+        """Check if place has attributes"""
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        self.assertTrue(hasattr(place, "user_id"))
+        self.assertTrue(hasattr(place, "name"))
+        self.assertTrue(hasattr(place, "description"))
+        self.assertTrue(hasattr(place, "number_rooms"))
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        self.assertTrue(hasattr(place, "max_guest"))
+        self.assertTrue(hasattr(place, "price_by_night"))
+        self.assertTrue(hasattr(place, "latitude"))
+        self.assertTrue(hasattr(place, "amenity_ids"))
+
+
+if __name__ == "__main__":
+    unittest.main()
