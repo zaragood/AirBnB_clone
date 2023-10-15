2f6177d9ae3fbcce8de3826760fd91988c2e7072
zaragood <azaragoodness52@gmail.com>
2023-10-15 15:21:08 +0000

    setattribut in if block

--- models/base_model.py
+++ models/base_model.py
@@ -10,17 +10,17 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializing BaseModel class"""
         if not kwargs:
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             models.storage.new(self)
 
         else:
             date_time = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, date_time)
-                if key != "__class__":
+                    setattr(self, key, datetime.strptime(value, date_time))
+                elif key != "__class__":
                     setattr(self, key, value)
 
     def __str__(self):
@@ -34,15 +34,16 @@ class BaseModel:
         """updates the public instance attribute updated_at
         with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
         of __dict__ of the instance:
         """
-        dict_obj = self.__dict__.copy()
-        dict_obj["__class__"] = self.__class__.__name__
+        dict_obj = {}
+        dict_obj.update(self.__dict__)
+        dict_obj.update({"__class__": self.__class__.__name__})
         dict_obj["created_at"] = self.created_at.isoformat()
         dict_obj["updated_at"] = self.updated_at.isoformat()
 
